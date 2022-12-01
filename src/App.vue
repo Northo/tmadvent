a904d2cecd36fde620dc287f2b151c92f5ba7792
Thorvald Molthe Ballestad <thorvald@simula.no>
2022-12-01 20:32:54 +0100

    Justify center

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ function opensOn(index) {
 
 <template>
   <h1 class="3xl my-5">Julekalender T&M</h1> 
-  <div class="flex flex-wrap gap-1">
+  <div class="flex flex-wrap justify-center gap-1">
     <FlipCard v-for="(day, index) in json" 
       :image="day.image"
       :day="index + 1"
